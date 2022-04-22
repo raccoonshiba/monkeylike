00a341ca3ec0143b6c902e29e9fa1828fbd2d83d
Julien <42862794+makethpanda@users.noreply.github.com>
2022-04-22 21:07:51 +0200

    stephen

--- engine/game.py
+++ engine/game.py
@@ -1,4 +1,11 @@
 '''Generates game object'''
-
-class Game:
-    pass
+from seed import genSeed
+from random import randint
+from floor import genFloor
+def gameGen(x):
+    game=[]
+    for i in range(genSeed(x)):
+        game.append(genFloor(randint(0,10000000)))
+    return game
+if __name__ == "__main__":
+    print(gameGen(1))
