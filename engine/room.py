cfc677ad9d4c1c6a450c1762ea9f81ca9c6f5aa7
Julien <42862794+makethpanda@users.noreply.github.com>
2022-04-27 18:00:30 +0200

    added inner wall generation

--- engine/room.py
+++ engine/room.py
@@ -10,15 +10,17 @@ def genRoom(x):
         room[i][0]="w"
         room[i][15]="w"
         room[15][i]="w"
-    for i in range(randint(0,1),randint(2,9)):
+    for i in range(randint(0,1),randint(2,9)):#object
         room[randint(1,14)][randint(1,14)]="o"
     room[15][8]="E"
     room[0][12]="E"
-    for i in range(randint(0,2),randint(2,6)):
+    for i in range(randint(0,2),randint(2,6)):#enemy
         room[randint(1,14)][randint(1,14)]="x"
-    for i in range(randint(0,2)):
+    for i in range(randint(0,2)):#terminal
         room[randint(1,14)][randint(1,14)]="T"
-    if randint(0,1000)<=5:
+    for i in range(randint(0,32)):#wall
+        room[randint(1,14)][randint(1,14)]="w"
+    if randint(0,1000)<=5:#anomaly
         room[randint(1,14)][randint(1,14)]="S"
         return room
     return room
