7156612e77c7bbb7b988b533bb8821cd40a541b5
Julien <42862794+makethpanda@users.noreply.github.com>
2022-05-20 11:03:10 +0200

    removed legacy

--- engine/player.py
+++ engine/player.py
@@ -7,35 +7,6 @@ class Player:
         self.defense = 1
         self.posx = 10
         self.posy = 10
-        self.image = pygame.image.load('monkey.png')
-
-    def move(self, direction, screen):
-        if direction == 'r':
-            self.posx += 10
-            screen.fill(pygame.Color("black"))
-            screen.blit(self.getImage(), (self.getPos()[0], self.getPos()[1]))
-
-        elif direction == 'l':
-            self.posx -= 10
-            screen.fill(pygame.Color("black"))
-            screen.blit(self.getImage(), (self.getPos()[0], self.getPos()[1]))
-
-        elif direction == 'u':
-            self.posy -= 10
-            screen.fill(pygame.Color("black"))
-
-            screen.blit(self.getImage(), (self.getPos()[0], self.getPos()[1]))
-
-        elif direction == 'd':
-            self.posy += 10
-            screen.fill(pygame.Color("black"))
-            screen.blit(self.getImage(), (self.getPos()[0], self.getPos()[1]))
-
-    def getPos(self):
-        return (self.posx, self.posy)
-    
-    def getImage(self):
-        return self.image
 
     def get_hp(self):
         return self.hp
