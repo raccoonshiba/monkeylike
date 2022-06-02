723b6b028fb222f1e96194066cff73a113637296
Julien <42862794+makethpanda@users.noreply.github.com>
2022-06-02 10:58:43 +0200

    happe

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from engine.game import Game
+import logging 
+import threading
+import time
+from pygame import mixer 
+from titlescreen import title
+    
+def music():
+    mixer.init()
+    mixer.music.load("spice/BeepBox-Song.mp3")
+    mixer.music.play()
+
 
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=title, args=())
+    y = threading.Thread(target=music, args=())
+    logging.info("Main    : before running thread")
+    x.start()
+    y.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
